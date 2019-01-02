837a93a143fa1f2c714afa1d9e4f0b2edf99d774
Raghavendra Vaidya <rkvaidya58@gmail.com>
2019-01-02 23:34:12 +0530

    Updated get data class with excel and properties methods

--- src/main/java/com/seleniumTA/Utilities/GetData.java
+++ src/main/java/com/seleniumTA/Utilities/GetData.java
@@ -1,17 +1,56 @@
 package com.seleniumTA.Utilities;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 public class GetData 
 {
 	static File fl;
+	static FileInputStream fis;
 	
 	public static String fromExcel(String fileName, String sheetName, int rIndex, int cIndex) 
 	{
 		String data = null;
 		fl = new File("./resources/"+fileName+".xlsx");
+		try
+		{
+			fis=  new FileInputStream(fl);
+			Workbook wb =WorkbookFactory.create(fis);
+			Sheet st= wb.getSheet(sheetName);
+			Row rw =st.getRow(rIndex);
+			Cell c =rw.getCell(cIndex);
+			DataFormatter dataformat = new DataFormatter();
+			data=dataformat.formatCellValue(c);		
+		}
 		
-		
+		catch(Exception e) 
+		{
+			e.printStackTrace();
+		}
+		return data;
+	}
+	
+	public static String fromProperties(String fileName, String key) {
+		String data = null;
+		fl= new File("./src/main/resources/"+fileName+".properties");
+		try 
+		{
+		fis = new FileInputStream(fl);
+		Properties prop = new Properties();
+		prop.load(fis);
+		data=(String)prop.getProperty(key);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
 		return data;
 	}
 }

15e8655ff54298a40f34dce264ab45122e9200b7
Raghavendra Vaidya <rkvaidya58@gmail.com>
2019-01-02 23:36:30 +0530

    updated utility class with screenshot capture, file upload, delete screenshot methods

--- src/main/java/com/seleniumTA/Utilities/Utility.java
+++ src/main/java/com/seleniumTA/Utilities/Utility.java
@@ -1,8 +1,18 @@
 package com.seleniumTA.Utilities;
 
+import java.awt.AWTException;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.regex.Pattern;
+
+import javax.imageio.ImageIO;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
@@ -28,12 +38,11 @@ public class Utility
 		if(!folder.exists()) 
 		{
 			folder.mkdir();
-				FileUtils.copyFile(source, destination);
+			FileUtils.copyFile(source, destination);
 		}
 		else if(folder.exists() && (!destination.exists())) 
 		{
-				FileUtils.copyFile(source, destination);
-			
+				FileUtils.copyFile(source, destination);		
 		}
 		else 
 		{
@@ -61,11 +70,89 @@ public class Utility
 			}
 			
 		}
-		else return;
-		
-		
+		else return;			
+	}
+	
+	/***Robot class screenshot method**/
+	
+	public static boolean robotScreenShot(ITestResult result) 
+	{
+		boolean success = false;
+		String methodName = result.getName();
+		String packageName = result.getInstanceName();
+		String nameCollection[] = packageName.split(Pattern.quote("."));
+		String classname = nameCollection[nameCollection.length-1];
+		File folder = new File("./screenshots/"+classname+"/"); 
+		File destination = new File("./screenshots/"+classname+"/"+methodName+".jpg");
+		Rectangle screenSize = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+		try {
+			Robot rbot = new Robot();
+			BufferedImage screenshot=rbot.createScreenCapture(screenSize);
+			if(!folder.exists()) 
+			{
+				folder.mkdir();
+				ImageIO.write(screenshot, "jpg", destination);
+				System.out.println("success");
+				return success = true;
+			}
+			else if(folder.exists()&& ((!destination.exists())))
+			{
+				ImageIO.write(screenshot, "jpg", destination);
+				System.out.println("success");
+				return success = true;
+			}
+			else return success;
+			
+		} catch (AWTException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return success;
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return success;
+		}	
 	}
 
+	/***robot class method to upload file*/
+	
+	public static boolean uploadFile(String filepath) 
+	{
+		boolean success = false;
+		
+		StringSelection stringSelection = new StringSelection(filepath);
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(stringSelection, null);
+ 
+        Robot robot = null;
+ 
+        try {
+            robot = new Robot();
+            robot.delay(250);
+            robot.keyPress(KeyEvent.VK_ENTER);
+            robot.keyRelease(KeyEvent.VK_ENTER);
+            robot.keyPress(KeyEvent.VK_CONTROL);
+            robot.keyPress(KeyEvent.VK_V);
+            robot.keyRelease(KeyEvent.VK_V);
+            robot.keyRelease(KeyEvent.VK_CONTROL);
+            robot.keyPress(KeyEvent.VK_ENTER);
+            robot.delay(150);
+            robot.keyRelease(KeyEvent.VK_ENTER);            
+            return success = true;
+        } 
+        catch (AWTException e) 
+        {
+            e.printStackTrace();
+            return success;
+        }
+        catch(Exception e) 
+        {
+        	e.printStackTrace();
+        	return success;
+        }
+         
+	}
 }
 
 

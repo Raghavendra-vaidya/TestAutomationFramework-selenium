908e7f6a91c7ac9a7d7846f96e2bc56638c053c5
Raghavendra Vaidya <rkvaidya58@gmail.com>
2019-01-02 23:34:54 +0530

    updated class with modified code

--- src/main/java/com/selenium/Configuration/CreateWebDriver.java
+++ src/main/java/com/selenium/Configuration/CreateWebDriver.java
@@ -1,5 +1,7 @@
 package com.selenium.Configuration;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -7,15 +9,22 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.ie.InternetExplorerOptions;
 
+import com.seleniumTA.Utilities.GetData;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class CreateWebDriver {
 
 	
 	public static WebDriver instance(String browser) 
 	{
 		WebDriver driver = null;
+		Integer imWaitTime=Integer.valueOf(GetData.fromProperties("configuration", "implicitWaitTime"));
+		String url = GetData.fromProperties("configuration", "URL");
 		switch (browser) 
 		{
 			case "ie":
+			WebDriverManager.iedriver().setup();
 			InternetExplorerOptions iop = new InternetExplorerOptions();
 			iop.introduceFlakinessByIgnoringSecurityDomains();
 			driver = new InternetExplorerDriver(iop);
@@ -23,27 +32,28 @@ public class CreateWebDriver {
 				break;
 			
 			case "chrome":
+			WebDriverManager.chromedriver().setup();
 			ChromeOptions cop = new ChromeOptions();
 			cop.setExperimentalOption("useAutomationExtension", false);
 			cop.addArguments("--start-maximized");
-			driver = new ChromeDriver();
+			driver = new ChromeDriver(cop);
 			
 				break;	
 		
 			case "firefox":
+				WebDriverManager.firefoxdriver().setup();
 				driver = new FirefoxDriver();
 				driver.manage().window().maximize();
 				break;
-			default:
-				
+			default:		
 				ChromeOptions cop2 = new ChromeOptions();
 				cop2.setExperimentalOption("useAutomationExtension", false);
 				cop2.addArguments("--start-maximized");
-				driver = new ChromeDriver();
+				driver = new ChromeDriver(cop2);
 				break;
 		}
-		
-		
+		driver.get(url);
+		driver.manage().timeouts().implicitlyWait(imWaitTime, TimeUnit.SECONDS);
 		return driver;
 	}
 }

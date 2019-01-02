47e6a4932b0e1515552c6d6e05adc0111fb23745
Raghavendra Vaidya <rkvaidya58@gmail.com>
2019-01-02 23:30:54 +0530

    updated ignore file with test-output folder

--- src/main/java/com/seleniumTA/Utilities/GetData.java
+++ src/main/java/com/seleniumTA/Utilities/GetData.java
@@ -1,5 +1,17 @@
 package com.seleniumTA.Utilities;
 
-public class GetData {
+import java.io.File;
 
+public class GetData 
+{
+	static File fl;
+	
+	public static String fromExcel(String fileName, String sheetName, int rIndex, int cIndex) 
+	{
+		String data = null;
+		fl = new File("./resources/"+fileName+".xlsx");
+		
+		
+		return data;
+	}
 }
--- src/test/java/com/selenium/tests/LoginpageTests.java
+++ src/test/java/com/selenium/tests/LoginpageTests.java
@@ -1,5 +1,39 @@
 package com.selenium.tests;
 
-public class LoginpageTests {
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 
+import com.selenium.Configuration.Base;
+import com.selenium.Configuration.CreateWebDriver;
+
+public class LoginpageTests extends Base
+{	
+	
+	@BeforeMethod
+	@Parameters({"browser"})
+	public void setUp(String browser) 
+	{
+		driver = CreateWebDriver.instance(browser);
+		//todo initiate page class object
+	}
+	
+	@Test(description="this test description", dependsOnMethods= {""}, priority=2)
+	public void Tc_login_01() {
+		
+	}
+	
+	
+	@Test(description="this test description", dependsOnMethods= {""}, priority=3)
+	public void Tc_login_02() {
+		
+	}
+	
+	
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+	
 }
